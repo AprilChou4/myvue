12622417fae315eb324a1893a400d93586fdae61
AprilChou4 <1575280371@qq.com>
2018-07-13 13:18:53 +0800

    first vue test

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <VFooter> </VFooter>
+  </div>
+</template>
+
+<script>
+import VFooter from './components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+    VFooter
+  }
+}
+</script>
+
+<style>
+@import './common/common.less';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+    <div class="g-footer">
+        <ul class="m-menu">
+            <li>
+                <p><i class="iconfont">&#xe620;</i></p>
+                <p>发现</p>
+            </li>
+            <li>
+                <p><i class="iconfont">&#xe620;</i></p>
+                <p>发现</p>
+            </li>
+            <li>
+                <p><i class="iconfont">&#xe620;</i></p>
+                <p>发现</p>
+            </li>
+            <li>
+                <p><i class="iconfont">&#xe620;</i></p>
+                <p>发现</p>
+            </li>
+            <li>
+                <p><i class="iconfont">&#xe620;</i></p>
+                <p>发现</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'vfooter'
+}
+</script>
+<style lang="less">
+.g-footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  .m-menu {
+      display:flex;
+      display:-webkit-flex;
+      li {
+          flex:1;
+          -webkit-flex:1
+      }
+  }
+}
+</style>
