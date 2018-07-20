eafe53cd3bb65d254c3d15e16df2457a19c7d660
AprilChou4 <1575280371@qq.com>
2018-07-20 14:34:30 +0800

    :partly_sunny:解决vux组件$t报错

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
+import vuexI18n from 'vuex-i18n'
 
 Vue.config.productionTip = false
 
@@ -11,6 +12,9 @@ Vue.config.productionTip = false
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  modules: {
+    i18n: vuexI18n.store
+  },
   state: {
     linkIndex: ''
   },
@@ -23,6 +27,18 @@ const store = new Vuex.Store({
 
   }
 })
+
+Vue.use(vuexI18n.plugin, store)
+
+const translationsEn = {
+  'content': 'This is some {type} content'
+}
+// translations can be kept in separate files for each language
+// i.e. resources/i18n/de.json.
+// add translations directly to the application
+Vue.i18n.add('en', translationsEn)
+// set the start locale to use
+Vue.i18n.set('en')
 
 /* eslint-disable no-new */
 new Vue({
